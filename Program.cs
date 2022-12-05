fc7d654c70d5611c6602c0a184ca61ef521fed22
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2022-12-05 21:34:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vjezbe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Unesite prvi broj");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Unesite drugi broj");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Zbroj=" + c);
+            Console.ReadKey();
+        }
+    }
+}
