04de956138c807005f4d752358d6e925ed4b7987
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2022-12-05 20:52:23 +0100

    Add files via upload

---
+++ Neki_Ispisi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hey!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bro\nCode");
+
+            Console.ReadKey();
+        }
+    }
+}
+
