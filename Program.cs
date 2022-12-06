0beabaae4624559befc31b4c49eed7e514ce0134
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2022-12-06 19:56:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Vjezbe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your name");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hi, {name}!");
+        }
+    }
+    
+}
